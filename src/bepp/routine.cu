6cde445b302eb80ec4e0bb5369e8af1539eb75f3
Andrew Marmaduke <arm77phoenix@gmail.com>
2014-03-20 09:53:26 -0400

    First pulloff profile attempt, unlikely to compile

--- src/bepp/routine.cu
+++ src/bepp/routine.cu
@@ -33,6 +33,161 @@ struct observer
   }
 };
 
+struct pp_observer
+{
+  vector_type<value_type>& prev;
+  vector_type
+
+};
+
+void pulloff_profile(json::Object& obj)
+{
+  parameter p(obj);
+  int size = p.m*p.n;
+
+  vector_type init(size*2+2);
+  thrust::device_ptr< value_type > d_delta =
+          thrust::device_malloc< value_type >(p.m);
+  for(int j = 0; j < p.m; ++j)
+  {
+    if(p.have_delta)
+      d_delta[j] = p.delta[j];
+    else
+      d_delta[j] = j;
+    for(int i = 0; i < p.n; ++i)
+    {
+      int index = i + p.n*j;
+      if(p.have_init)
+      {
+        init[index] = p.init[index];
+        init[index+size] = p.init[index+size];
+      }else
+      {
+        init[index] = d_delta[j];
+        init[index+size] = i+1;
+      }
+    }
+  }
+  if(p.have_init)
+  {
+    init[0+2*size] = p.init[0+2*size];
+    init[1+2*size] = p.init[1+2*size];
+    free(p.delta);
+  }
+  else
+  {
+    init[0+2*size] = p.sub_x;
+    init[1+2*size] = p.sub_y;
+  }
+  p.delta = d_delta.get();
+
+  cudaEvent_t start, stop;
+  float timer;
+
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+  cudaEventRecord(start,0);
+
+  std::vector<value_type> times;
+  times.push_back(9); times.push_back(10);
+
+  json::Array* grid = new json::Array();
+
+  double linear_step = 5;
+  int output_i = 1;
+  double magnitude = 10;
+  double lower_bound = 0;
+  double upper_bound = 100;
+  bool have_upper = false, have_lower = false;
+
+  double dt = .1, t0 = 0;
+  double theta_begin = 0, theta_end = 45, theta_h = 5;
+
+  for(int t = theta_begin; t <= theta_end; t+=theta_h)
+  {
+    bool found = false;
+    have_lower = have_upper = false;
+
+    if(t != theta_begin)
+    {
+      linear_step = 1;
+    }
+
+    while(not found)
+    {
+      bool done = false;
+      int outcome = -1;
+      while(not done)
+      {
+        double l = -magnitude*sin(PI*t/180);
+        double m = magnitude*cos(PI*t/180);
+
+        integrate_times(make_controlled(p.abstol, p.reltol, stepper_type()),
+          F, init, times.begin(), times.end(), dt, O);
+
+        double equil = sqrt(thrust::inner_product(init.data(),
+                                            init.data()+2*size,init.data(),0.));
+        double adhesion = sqrt(thrust::inner_product(init.data()+2*size,
+                                  init.data()+2*size+2,init.data()+2*size,0.));
+        double d = sqrt(l*l + m*m);
+
+        if(abs(adhesion - d) < abstol)
+        {
+          done = true;
+          json::Array* temp = new json::Array();
+          temp.push_back(l); temp.push_back(m); temp.push_back(1);
+          grid.push_back(temp);
+          outcome = 1;
+        }else if(equil < abstol and adhesion < abstol)
+        {
+          done = true;
+          json::Array* temp = new json::Array();
+          temp.push_back(l); temp.push_back(m); temp.push_back(0);
+          grid.push_back(temp);
+          outcome = 0;
+        }
+      }
+
+      if(have_upper and have_lower and abs(upper_bound - lower_bound) < .5)
+      {
+        found = true;
+      }
+
+      if(not have_lower or not have_upper)
+      {
+        if(outcome == 1) //Pulled off
+        {
+          lower_bound = magnitude;
+          have_lower = true;
+          if(not have_upper)
+            magnitude = magnitude + linear_step;
+        }else if(outcome == 0) // Adhered
+        {
+          upper_bound = magnitude;
+          have_upper = true;
+          if(not have_lower)
+            magnitude = magnitude - linear_step;
+        }
+      }
+
+      if(have_lower and have_upper)
+      {
+        if(outcome == 1) //Pulled off
+          lower_bound = magnitude;
+        else if(outcome == 0) // Adhered
+          upper_bound = magnitude;
+        magnitude = lower_bound + (upper_bound - lower_bound) / 2;
+      }
+      ++output_i;
+    }
+  }
+  obj["grid"] = grid;
+  std::ofstream File("pp.json");
+  json::print(File,obj);
+  std::ofstream File2("/home/marmaduke/pp.json");
+  json::print(File2,obj);
+}
+
 void ode_test(json::Object& obj)
 {
   parameter p(obj);
@@ -41,38 +196,38 @@ void ode_test(json::Object& obj)
   vector_type init(size*2+2);
   thrust::device_ptr< value_type > d_delta =
           thrust::device_malloc< value_type >(p.m);
-	for(int j = 0; j < p.m; ++j)
+  for(int j = 0; j < p.m; ++j)
   {
-		if(p.have_delta)
-			d_delta[j] = p.delta[j];
-		else
-    	d_delta[j] = j;
+    if(p.have_delta)
+      d_delta[j] = p.delta[j];
+    else
+      d_delta[j] = j;
     for(int i = 0; i < p.n; ++i)
     {
       int index = i + p.n*j;
-			if(p.have_init)
-			{
-				init[index] = p.init[index];
-				init[index+size] = p.init[index+size];
-			}else
-			{
-      	init[index] = d_delta[j];
-      	init[index+size] = i+1;
-    	}
-		}
+      if(p.have_init)
+      {
+        init[index] = p.init[index];
+        init[index+size] = p.init[index+size];
+      }else
+      {
+        init[index] = d_delta[j];
+        init[index+size] = i+1;
+      }
+    }
   }
-	if(p.have_init)
-	{
-		init[0+2*size] = p.init[0+2*size];
-		init[1+2*size] = p.init[1+2*size];
-		free(p.delta);
-	}
+  if(p.have_init)
+  {
+    init[0+2*size] = p.init[0+2*size];
+    init[1+2*size] = p.init[1+2*size];
+    free(p.delta);
+  }
   else
-	{
-		init[0+2*size] = p.sub_x;
-  	init[1+2*size] = p.sub_y;
+  {
+    init[0+2*size] = p.sub_x;
+    init[1+2*size] = p.sub_y;
   }
-	p.delta = d_delta.get();
+  p.delta = d_delta.get();
 
   cudaEvent_t start, stop;
   float timer;
@@ -88,15 +243,15 @@ void ode_test(json::Object& obj)
   times.push_back(9);
   times.push_back(10);
 
-	force_functor F(p);
+  force_functor F(p);
   observer O(v,vp);
 
-	value_type dt = .1;
+  value_type dt = .1;
   value_type t0 = 0;
-	//integrate_times(make_controlled(p.abstol, p.reltol, stepper_type()),
+  //integrate_times(make_controlled(p.abstol, p.reltol, stepper_type()),
   //              F, init, times.begin(), times.end(), dt, O);
-	integrate_n_steps(make_controlled(p.abstol, p.reltol, stepper_type()),
-								F, init, t0, dt, 10000, O);
+  integrate_n_steps(make_controlled(p.abstol, p.reltol, stepper_type()),
+                F, init, t0, dt, 10000, O);
 
   cudaEventRecord(stop,0);
   cudaEventSynchronize(stop);
@@ -106,26 +261,26 @@ void ode_test(json::Object& obj)
 
   for(int i = 0; i < vp.size(); ++i)
   {
-		json::Array* temp = new json::Array;
+    json::Array* temp = new json::Array;
     value_type* s = vp[i];
-		std::stringstream ss;
-		ss << "t:" << v[i];
-		std::string temp2 = ss.str();
+    std::stringstream ss;
+    ss << "t:" << v[i];
+    std::string temp2 = ss.str();
     for(int j = 0; j < 2*size+2; ++j)
     {
-			json::Number* num = new json::Number;
-			num->val = s[j];
-			temp->push_back(num);
+      json::Number* num = new json::Number;
+      num->val = s[j];
+      temp->push_back(num);
     }
-		obj[temp2] = temp;
+    obj[temp2] = temp;
   }
 
-	std::cout << "time: " << timer << " ms " << (timer/1000) << " s" << std::endl;
+  std::cout << "time: " << timer << " ms " << (timer/1000) << " s" << std::endl;
 
-	std::ofstream File("output.json");
-	json::print(File,obj);
-	std::ofstream File2("/home/marmaduke/output.json");
-	json::print(File2,obj);
+  std::ofstream File("output.json");
+  json::print(File,obj);
+  std::ofstream File2("/home/marmaduke/output.json");
+  json::print(File2,obj);
 }
 
 int main()
